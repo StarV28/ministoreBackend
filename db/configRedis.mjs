6c8a723997db18a02d200db4f6d46ee2bf154509
Vlad Starovoit <vladimirstar80@gmail.com>
2025-05-23 20:01:40 +0200

    working with a Redis secret key

--- db/configRedis.mjs
+++ db/configRedis.mjs
@@ -1,6 +1,10 @@
 import { createClient } from 'redis'
+import dotenv from 'dotenv'
+dotenv.config()
 
-const redisClient = createClient()
+const redisClient = createClient({
+  url: process.env.REDIS_URL,
+})
 
 redisClient.on('error', (err) => console.error('❌ Redis Client Error:', err))
 
